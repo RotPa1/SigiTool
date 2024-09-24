98ad5455b70a8634f149f204c3be634554dbdc40
Rotem Paz <rotemypaz@gmail.com>
2024-09-24 17:58:43 +0100

    Fix logo path for static files

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, url_for
 import pandas as pd
+from whitenoise import WhiteNoise
 
 app = Flask(__name__)
+app.wsgi_app = WhiteNoise(app.wsgi_app, root='static/')
 
 # Load the dataset once when the app starts
 data = pd.read_excel('JaundiceTH.xlsx')
